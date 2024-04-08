c5901c70a36819d59563930e7300e6b164517d1f
Nao <thanakorn55768@gmail.com>
2024-04-08 11:53:25 +0700

    edit playlist.php

--- playlist.php
+++ playlist.php
@@ -50,6 +50,7 @@ if(isset($_POST['save_list'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>playlist</title>
+   <title>playlist</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
